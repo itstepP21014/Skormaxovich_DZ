0a154d8edb46cbef370a8c9c47f7594a6a2ffd07
unknown <C:\Users\HP\AppData\Roaming\The Bat!>
2014-11-30 14:58:13 +0300

    asfd

---
+++ 4ETbIPE_4UCJIA/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int oguH,gBA,Tpu,cheTbIpe;
+printf("Enter cheTbIpe digits:\n");
+scanf("%d%d%d%d",&oguH,&gBA,&Tpu,&cheTbIpe);
+printf("Result:\n\n");
+printf("%d\n %d\n  %d\n   %d\n",oguH*2,gBA*2,Tpu*2,cheTbIpe*2);
+return 0;
+}
---
+++ CTUX/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ printf("\tAnd The Moon And The Stars And The World\n");
+
+ printf("\t\tby Charles Bukowski\n");
+
+ printf("Long walks at night--\n");
+
+ printf("that’s what good for the soul:\n");
+
+ printf("peeking into windows\n");
+
+ printf("watching tired housewives\n");
+
+ printf("trying to fight off\n");
+
+ printf("their beer-maddened husbands.\n");
+
+    return 0;
+}

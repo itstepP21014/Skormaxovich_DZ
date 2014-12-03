e04a49c7ba684ecb881b0f9a79d710700547efeb
unknown <C:\Users\HP\AppData\Roaming\The Bat!>
2014-12-03 14:17:43 +0300

    asf

---
+++ nomer3/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+char a[],b[],c[];
+int main()
+{
+double anumb=-5.234;
+double bnumb=-6.324;
+double cnumb=-9.527;
+double dnumb=9.227;
+char a[]="Olga";
+char b[]="Roman";
+char c[]="Jo";
+char d[]="Ira";
+printf("%7s | %+.3f\n",a,anumb);
+printf("%7s | %+.3f\n",b,bnumb);
+printf("%7s | %+.3f\n",c,cnumb);
+printf("%7s | %+.3f\n",d,dnumb);
+    return 0;
+}

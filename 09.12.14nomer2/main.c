775ac7dcb785ed2bd9d68e424caaffbc454494de
skormaxovich <myadel2006@mail.ru>
2015-01-08 16:32:06 +0300

    Update main.c

--- 09.12.14nomer2/main.c
+++ 09.12.14nomer2/main.c
@@ -2,14 +2,32 @@
 #include <stdlib.h>
 
 int main()
-{ int x,p;
-printf ("write x");
-scanf(x);
-printf("write p");
-scanf(p);
-for (int i=1;i<=p;i=i+1)
-{x=x*x;
- }
- printf("%d",x);
+{ int x,p,proizvedenie;
+double a;
+scanf("%d",&x);
+
+scanf("%d",&p);
+
+proizvedenie=x;
+a=1/x;
+if (p>=0)
+{
+    for(int i=1;i<p;i=i+1)
+    { proizvedenie=proizvedenie*x;
+
+
+    }
+}
+    else
+    {
+        a=a/x;
+
+
+    }
+
+
+
+ printf(" %d ",proizvedenie);
+ printf(" %d  ",a);
 return 0;
 }

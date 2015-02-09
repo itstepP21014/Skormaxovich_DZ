c9da5b4ce151d9a2ad9958131a19f26bed210845
skormaxovich <myadel2006@mail.ru>
2015-02-09 12:24:56 +0000

    Update main.c

--- krestik_vs_nolik_battle_of_the_gods/main.c
+++ krestik_vs_nolik_battle_of_the_gods/main.c
@@ -28,7 +28,7 @@ for (int i=1;i<10;i=i+1){
     printf("Enter stolbec:");
     scanf("%d",&stolbec);
     field [strochka][stolbec]=a;
-    system("cls");}
+    system("clear");}
 
 
 
@@ -38,3 +38,43 @@ for (int i=1;i<10;i=i+1){
 
     return 0;
 }
+
+
+
+//second version program!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//#include <stdio.h>
+//#include <stdlib.h>
+
+//int main()
+//{
+//int strochka,stolbec,a;
+//char field[4][4]={32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32};
+//for (int i=1;i<10;i=i+1){
+  //  if (i%2==1)
+    //{
+      //  a=88;
+    //}
+    //else
+    //{
+      //  a=79;
+    //}
+    //printf("  1  2  3\n");
+    //printf("1 ");
+    //for (int c=1;c<4;c=c+1){
+    //printf("%c  ",field[1][c]);}
+    //printf("\n2 ");
+    //for (int c=1;c<4;c=c+1){
+    //printf("%c  ",field[2][c]);}
+    //printf("\n3 ");
+    //for (int c=1;c<4;c=c+1){
+    //printf("%c  ",field[3][c]);}
+    //printf("\nEnter strochka:");
+    //scanf("%d",&strochka);
+
+    //printf("Enter stolbec:");
+    //scanf("%d",&stolbec);
+    //field [strochka][stolbec]=a;
+    //system("clear");}
+//return 0;
+//}
+
